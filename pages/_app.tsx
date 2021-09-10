4052ff1bc402f69fecd2bc04a444c2fe23793590
n00nietzsche <n00nietzsche@gmail.com>
2021-09-10 23:52:18 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+import Head from 'next/head'
+import { GlobalStyles } from 'twin.macro'
+import { AppProps } from 'next/app'
+
+const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>
+        Nextjs App with TypeScript, ESlint, Jest, Emotion, Tailwind and Twin
+      </title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>
+)
+
+export default App
